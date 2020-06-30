74640f106c6e2ac8833a31d062d14340595382d0
demi-werkzam <d.werkzam@gmail.com>
2020-06-30 21:10:37 +0200

    models for visit, like, user and restaurant have been added

---
+++ models/restaurant.js
@@ -0,0 +1,42 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const restaurant = sequelize.define(
+    "restaurant",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      website: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      instagram: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      address: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      latitude: {
+        type: DataTypes.FLOAT,
+      },
+      longitude: {
+        type: DataTypes.FLOAT,
+      },
+    },
+    {}
+  );
+  restaurant.associate = function (models) {
+    // associations can be defined here
+  };
+  return restaurant;
+};
---
+++ models/user.js
@@ -0,0 +1,31 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define(
+    "user",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      userName: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {}
+  );
+  user.associate = function (models) {
+    // associations can be defined here
+  };
+  return user;
+};
